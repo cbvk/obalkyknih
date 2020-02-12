2c80173c96e6cf6b8c7667d29ac4f0b5cf9b09e4
cosmo-cz <hagara@cosmotron.cz>
2020-02-12 15:35:28 +0100

    Pridane importovanie celeho priecinka

--- backend/okczd/bin/harvest_trans_mzk.py
+++ backend/okczd/bin/harvest_trans_mzk.py
@@ -1,35 +1,97 @@
 from pymongo import MongoClient
 import argparse
+from tinydb import TinyDB, Query
+from os import listdir
+from os.path import isfile, join
+import os
 
 
-def harvest_trans_mzk(export, db, collection):
+def harvest_trans_mzk_directory(directory, db, collection, work_dir=None):
     """
-    Metoda pre ukladanie zaznamov zo suboru 'export' do MongoDB
-    :param export: subor v ktorom su zaznamy - moze byt objekt file alebo cesta k suboru
-    :param db: databaza do ktorej sa ulozia zaznamy
-    :param collection: kolekcia do ktorej sa ulozia zaznamy
-    :return: pocet ulozenych zaznamov
+    Importuje vestky .txt subory v zadanom priecinku, ak este nie su v tinyDB
+    tinyDB uklada zaznamy do db.json v priecinku v ktorom je tento skript
+    :param work_dir: Kam sa uklada subor db.json
+    :param directory: Priecinok z ktoreho sa budu subory importovat
+    :param db: Mongo databaza kam sa maju zaznamy naimportovat
+    :param collection: Mongo colekcia kam sa maju zaznamy naimportovat
+    :return:
     """
+    if work_dir is None:
+        work_dir = os.path.dirname(os.path.realpath(__file__))
+    files = [f for f in listdir(directory) if (isfile(join(directory, f)) and f.endswith(".txt"))]
+    tinyDB = TinyDB(work_dir + '/db.json')
     # pripojenie na Mongo
     client = MongoClient(port=27017)
     db = client[db]
     dbTrans = db[collection]
+    # prejdenie vsetkych suborov v priecinku
+    for file in files:
+        if not check_if_already_imported(file, tinyDB):
+            # subor sa naimportuje ak este nie je v tinyDB
+            print("Importujem subor:" + join(directory, file))
+            imp, not_imp = harvest_trans_mzk(join(directory, file), db, dbTrans, work_dir)
+            print("Importovanych zaznamov: " + str(imp))
+            print("Neimportovanych zaznamov " + str(not_imp))
+            # ulozenie do tinyDB ak vsetko zbehlo
+            tinyDB.insert({'name': file})
+
+
+def check_if_already_imported(file_name, tinyDB):
+    """
+    Skontroluje ci uz bol subor naimportovany
+    :param file_name: Subor ktory sa zistuje ci bol uz naimportovany
+    :param tinyDB: tinyDB
+    :return: Vrati True ak uz subor je v tinyDB inak vrati False
+    """
+    files = Query()
+    result = tinyDB.search(files.name == file_name)
+    if not result:
+        return False
+    else:
+        return True
+
+
+def harvest_trans_mzk(export, db, dbTrans, work_dir):
+    """
+    Metoda pre ukladanie zaznamov zo suboru 'export' do MongoDB
+    :param work_dir: Priecinok ka sa uklada not_imported.txt
+    :param export: subor v ktorom su zaznamy - moze byt objekt file alebo cesta k suboru
+    :param db: databaza do ktorej sa ulozia zaznamy
+    :param dbTrans: kolekcia do ktorej sa ulozia zaznamy
+    :return: pocet ulozenych zaznamov
+    """
+    if work_dir is None:
+        work_dir = os.path.dirname(os.path.realpath(__file__))
+    # pripojenie na Mongo
+    if isinstance(db, str):
+        client = MongoClient(port=27017)
+        db = client[db]
+        dbTrans = db[dbTrans]
     sigla = "CBA001"
     if isinstance(export, str):
         # otvorenie suboru
         file = open(export, 'r')
     else:
         file = export
+    file_not_imported = open(work_dir + "/not_imported.txt", "a+")
     i = 0
-    # oxtrakcia zaznamov kde jeden riadok = jeden zaznam
+    imported = 0
+    not_imported = 0
+    # extrakcia zaznamov kde jeden riadok = jeden zaznam
     for line in file.readlines():
-        line = line.strip('\n')
-        result = parse_line(line)
-        result['sigla'] = sigla
-        dbTrans.insert_one(result)
         i += 1
+        try:
+            line = line.strip('\n')
+            result = parse_line(line)
+            result['sigla'] = sigla
+            dbTrans.insert_one(result)
+            imported += 1
+        except Exception as exc:
+            not_imported += 1
+            print("Line" + str(i) + " " + line + "was not imported because of exception: " + str(exc))
+            file_not_imported.write(line + '\n')
     file.close()
-    return i
+    return imported, not_imported
 
 
 def parse_line(line):
@@ -76,9 +138,14 @@ def parse_line(line):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Skript pre import dat o pozickach do MongoDB')
-    parser.add_argument('file', help='Subor z ktoreho sa importuju data')
+    parser.add_argument('path', help='Ak je cesta subor importuje sa jeden subor, ak je cesta priecinok importuju sa '
+                                     'vsetky subory v priecinku')
     args = parser.parse_args()
-    export = args.file
-    print("Importujem subor:" + export)
-    number = harvest_trans_mzk(export, 'okczd', 'trans')
-    print("Importovanych zaznamov: " + str(number))
+    path = args.path
+    if isfile(path):
+        print("Importujem subor:" + path)
+        imported, not_imported = harvest_trans_mzk(path, 'okczd', 'trans', None)
+        print("Importovanych zaznamov: " + str(imported))
+        print("Neimportovanych zaznamov " + str(not_imported))
+    else:
+        harvest_trans_mzk_directory(path, 'okczd', 'trans')
--- backend/okczd/tests/test_harvest_trans_mzk.py
+++ backend/okczd/tests/test_harvest_trans_mzk.py
@@ -1,18 +1,29 @@
 import unittest
-from bin.harvest_trans_mzk import harvest_trans_mzk
+from bin.harvest_trans_mzk import harvest_trans_mzk, harvest_trans_mzk_directory
 from pymongo import MongoClient
+import os
 
 
 class TestHarvestTransMZK(unittest.TestCase):
     def test_harvest_trans_mzk(self):
         try:
             with open("mzk-test2.txt", 'r') as file:
-                number = harvest_trans_mzk(file, 'test_db', 'trans')
-                print(number)
-            self.assertEqual(5807, number)
+                imported, not_imported = harvest_trans_mzk(file, 'test_db', 'trans', '.')
+                print(imported)
+            self.assertEqual(5807, imported)
         finally:
             client = MongoClient(port=27017)
             client.drop_database('test_db')
+            os.remove("./not_imported.txt")
+
+    def test_harvest_mzk_directory(self):
+        try:
+            harvest_trans_mzk_directory('.', 'test_db', 'trans', '.')
+        finally:
+            client = MongoClient(port=27017)
+            client.drop_database('test_db')
+            os.remove("./not_imported.txt")
+            os.remove("./db.json")
 
 
 if __name__ == '__main__':
