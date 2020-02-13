0a67bd2e24c4d2a5c327fbf10090e7e2516b32f8
cosmo-cz <hagara@cosmotron.cz>
2020-02-13 11:28:39 +0100

    Update test_harvest_trans_mzk.py

--- backend/okczd/tests/test_harvest_trans_mzk.py
+++ backend/okczd/tests/test_harvest_trans_mzk.py
@@ -6,24 +6,26 @@ import os
 
 class TestHarvestTransMZK(unittest.TestCase):
     def test_harvest_trans_mzk(self):
+        work_dir = os.path.dirname(os.path.realpath(__file__))
         try:
             with open("mzk-test2.txt", 'r') as file:
-                imported, not_imported = harvest_trans_mzk(file, 'test_db', 'trans', '.')
+                imported, not_imported = harvest_trans_mzk(file, 'test_db', 'trans', work_dir)
                 print(imported)
             self.assertEqual(5807, imported)
         finally:
             client = MongoClient(port=27017)
             client.drop_database('test_db')
-            os.remove("./not_imported.txt")
+            os.remove(work_dir + "/not_imported.txt")
 
     def test_harvest_mzk_directory(self):
+        work_dir = os.path.dirname(os.path.realpath(__file__))
         try:
-            harvest_trans_mzk_directory('.', 'test_db', 'trans', '.')
+            harvest_trans_mzk_directory('.', 'test_db', 'trans', work_dir)
         finally:
             client = MongoClient(port=27017)
             client.drop_database('test_db')
-            os.remove("./not_imported.txt")
-            os.remove("./db.json")
+            os.remove(work_dir + "/not_imported.txt")
+            os.remove(work_dir + "/db.json")
 
 
 if __name__ == '__main__':
